f2ee5daeeeb0893317f8647a2398eecc1dd5ea08
Oleg Kiselev <e5ash.bro@gmail.com>
2022-06-27 18:14:59 +0300

    add scss blocks

--- io.js
+++ io.js
@@ -1,4 +1,4 @@
-const PATH    = require('./node/path.js');
+const PATH = require('./node/path.js');
 const Creator = require('./node/creator.js');
 const Watcher = require('./node/watcher.js');
 
@@ -10,29 +10,27 @@ var argv = {
 }
 
 if (process.argv.length > 1) {
-	switch (argv.flag){
+	switch (argv.flag) {
 		case '-cb':
 			var fileBlocks = PATH.PUG + 'blocks.txt';
 			for (let item of argv.list) {
 				var name = item;
-				new Creator(name, PATH.BLOCKS, ['.scss', '.pug', '.js'], {true: `Блок ${name} успешно создан`, false: `Блок ${name} уже имеется`})
+				new Creator(name, PATH.BLOCKS, ['.scss', '.pug', '.js'], { true: `Блок ${name} успешно создан`, false: `Блок ${name} уже имеется` })
 				var blocks = fs.readFileSync(fileBlocks);
 				fs.writeFileSync(fileBlocks, blocks + `//- ${item} \n+style('${item}', 'blocks')\ninclude ../../blocks/${item}/${item}\n\n`);
-			}	
+			}
 			break;
 		case '-cc':
 			for (let item of argv.list) {
-				var name = item.replace(/[\W\d]/g,'')
-				new Creator(name, PATH.COMPS, ['.scss', '.pug', '.js'], {true: `Компонент ${name} успешно создан`, false: `Компонент ${name} уже имеется`})				
-			}	
+				var name = item.replace(/[\W\d]/g, '')
+				new Creator(name, PATH.COMPS, ['.scss', '.pug', '.js'], { true: `Компонент ${name} успешно создан`, false: `Компонент ${name} уже имеется` })
+			}
 			break;
 		case '-cp':
 			for (let item of argv.list) {
 				var name = item;
-				new Creator(name, PATH.PAGES, ['.pug'], {true: `Страница ${name} успешно создан`, false: `Страница ${name} уже имеется`})				
-				var opn = require('opn');
-				opn('localhost:3000/' + name + '.html');
-			}	
+				new Creator(name, PATH.PAGES, ['.pug'], { true: `Страница ${name} успешно создан`, false: `Страница ${name} уже имеется` })
+			}
 			break;
 		case '-w':
 			new Watcher();
